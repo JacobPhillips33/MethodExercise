a193884d4b330726a531648b532b8b57b3a26271
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-10 11:11:32 -0500

    utilized params to accept a range of arguments in calculations

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -4,29 +4,64 @@ namespace MethodExercise
 {
     internal class Program
     {
-        public static int Add(int num1, int num2)
+        public static int Add(params int[] methodAdd)
         {
-            return num1 + num2;
+            int totalAdd = 0;
+
+            foreach(int Add in methodAdd)
+            {
+                totalAdd += Add;
+            }
+
+            return totalAdd;
         }
-        public static int Subtract(int num1, int num2)
+        public static int Subtract(params int[] methodSubtract)
         {
-            return num1 - num2;
+            int totalSubtract = 0;
+
+            foreach(int Subtract in methodSubtract)
+            {
+                totalSubtract -= Subtract;
+            }
+
+            return totalSubtract;
         }
-        public static int Multiply(int num1, int num2)
+        public static int Multiply(params int[] methodMultiply)
         {
-            return num1 * num2;
+            int totalMultiply = 1;
+
+            foreach(int Multiply in methodMultiply)
+            {
+                totalMultiply *= Multiply;
+            }
+
+            return totalMultiply;
         }
-        public static int Divide(int num1, int num2)
+        public static double Divide(params double[] methodDivide)
         {
-            return num1 / num2;
+            double totalDivide = 1000;
+
+            foreach(double Divide in methodDivide)
+            {
+                totalDivide /= Divide;
+            }
+
+            return totalDivide;
         }
-        public static int Modulus(int num1, int num2)
+        public static int Modulus(params int[] methodModulus)
         {
-            return num1 % num2;
+            int totalModulus = 23;
+
+            foreach(int Modulus in methodModulus)
+            {
+                totalModulus %= Modulus;
+            }
+
+            return totalModulus;
         }
         static void Main(string[] args)
         {
-            //-------------------Exercise 1-------------------------------------------
+            ////-------------------Exercise 1-------------------------------------------
             Console.WriteLine("Hello! What is your name?");
             var name = Console.ReadLine();
 
@@ -36,7 +71,7 @@ namespace MethodExercise
             Console.WriteLine($"{color} is my favorite color too! What is your favorite animal?");
             var animal = Console.ReadLine();
 
-            Console.WriteLine($"{animal}s are so cool are they? What is your favorite band?");
+            Console.WriteLine($"{animal}s are so cool aren't they? What is your favorite band?");
             var band = Console.ReadLine();
 
             Console.WriteLine($"Wow, that is crazy! You might not believe it, but I was wearing a {color} shirt when I " +
@@ -44,20 +79,21 @@ namespace MethodExercise
 
             //-------------------Exercise 2-------------------------------------------
 
-            var methodAdd = Add(6, 2);
+            var methodAdd = Add(6, 2, 21, 42);
             Console.WriteLine(methodAdd);
 
-            var methodSubtract = Subtract(6, 2);
+            var methodSubtract = Subtract(6, 2, 12, 33);
             Console.WriteLine(methodSubtract);
 
-            var methodMultiply = Multiply(6, 2);
+            var methodMultiply = Multiply(6, 2, 39, 12, 42, 102);
             Console.WriteLine(methodMultiply);
 
             var methodDivide = Divide(6, 2);
             Console.WriteLine(methodDivide);
 
-            var methodModulus = Modulus(23, 6);
+            var methodModulus = Modulus(6, 3);
             Console.WriteLine(methodModulus);
+                      
         }
     }
 }
