81af31881450c355f23d9494bfa196077633561e
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-09 21:47:38 -0500

    uncommented exercise 1

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -27,20 +27,20 @@ namespace MethodExercise
         static void Main(string[] args)
         {
             //-------------------Exercise 1-------------------------------------------
-            //Console.WriteLine("Hello! What is your name?");
-            //var name = Console.ReadLine();
+            Console.WriteLine("Hello! What is your name?");
+            var name = Console.ReadLine();
 
-            //Console.WriteLine($"Hi, {name}. What is your favorite color?");
-            //var color = Console.ReadLine();
+            Console.WriteLine($"Hi, {name}. What is your favorite color?");
+            var color = Console.ReadLine();
 
-            //Console.WriteLine($"{color} is my favorite color too! What is your favorite animal?");
-            //var animal = Console.ReadLine();
+            Console.WriteLine($"{color} is my favorite color too! What is your favorite animal?");
+            var animal = Console.ReadLine();
 
-            //Console.WriteLine($"{animal}s are so cool are they? What is your favorite band?");
-            //var band = Console.ReadLine();
+            Console.WriteLine($"{animal}s are so cool are they? What is your favorite band?");
+            var band = Console.ReadLine();
 
-            //Console.WriteLine($"Wow, that is crazy! You might not believe it, but I was wearing a {color} shirt when I " +
-            //    $"met a {animal} named {name} at a concert by {band}... What are the odds?");
+            Console.WriteLine($"Wow, that is crazy! You might not believe it, but I was wearing a {color} shirt when I " +
+                $"met a {animal} named {name} at a concert by {band}... What are the odds?");
 
             //-------------------Exercise 2-------------------------------------------
 
